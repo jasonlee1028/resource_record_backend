c263c3da651eefe1fe129039fbf72e41d9e355ad
lijie <jie.li@equotaenergy.com>
2019-01-06 11:42:44 +0800

    add static files url

--- resource_record_backend/urls.py
+++ resource_record_backend/urls.py
@@ -15,17 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
-
-from resource_record_backend import settings
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
